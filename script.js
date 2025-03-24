737be4c65ab786abd994fe06def03982f14a49ab
Sammy01finky <110358789+Sammy01finky@users.noreply.github.com>
2025-03-23 23:32:24 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const mobileNav = document.querySelector('.mobile-nav');
-
-menuToggle.addEventListener('click', () => {
-    mobileNav.classList.toggle('show');
-});
